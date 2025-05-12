ead80d8576b93fe2f54c76c40af6e6f9bec7bbb5
RikMuller420 <rikmuller420@gmail.com>
2025-05-12 11:26:43 +0300

    Убраны зоны стики банера, отрезанные от экрана

--- Assets/Source/Scripts/Screen Orientation/UIOrientationChanger.cs
+++ Assets/Source/Scripts/Screen Orientation/UIOrientationChanger.cs
@@ -3,14 +3,11 @@ using UnityEngine;
 public class UIOrientationChanger : MonoBehaviour
 {
     [SerializeField] private ScreenSizeChangeTracker _screenSizeChangeTracker;
-    [SerializeField] private RectTransform _gameZone;
-    [SerializeField] private RectTransform _bannerZone;
     [SerializeField] private GameObject _boostsZoneVertical;
     [SerializeField] private GameObject _boostsZoneHorizontal;
 
     private bool _isVertical = true;
 
-    private float _banerAnchorsSize = 0.1f;
 
     private void OnEnable()
     {
@@ -35,23 +32,6 @@ public class UIOrientationChanger : MonoBehaviour
 
     private void UpdateOrientation()
     {
-        if (_isVertical)
-        {
-            _bannerZone.anchorMin = Vector2.zero;
-            _bannerZone.anchorMax = new Vector2(1, _banerAnchorsSize);
-
-            _gameZone.anchorMin = new Vector2(0, _banerAnchorsSize);
-            _gameZone.anchorMax = Vector2.one;
-        }
-        else
-        {
-            _bannerZone.anchorMin = new Vector2(1f - _banerAnchorsSize, 0);
-            _bannerZone.anchorMax = Vector2.one;
-
-            _gameZone.anchorMin = Vector2.zero;
-            _gameZone.anchorMax = new Vector2(1f - _banerAnchorsSize, 1);
-        }
-
         _boostsZoneVertical.SetActive(_isVertical);
         _boostsZoneHorizontal.SetActive(_isVertical == false);
     }
